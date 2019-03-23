545e0569267960e3d9c72e1d50296c6a138ad3f2
Ben Therien <cs135cid@yahoo.com>
2019-03-23 09:14:58 -0400

    Create Clear-HostLine.psm1

---
+++ Clear-HostLine.psm1
@@ -0,0 +1,19 @@
+﻿Function Clear-HostLine {
+    Param (
+	    [Parameter(Position=1)]
+	    [int32]$Count=1
+    )
+
+    $CurrentLine  = $Host.UI.RawUI.CursorPosition.Y
+    $ConsoleWidth = $Host.UI.RawUI.BufferSize.Width
+
+    $Lines = 1
+    for ($Lines; $Lines -le $Count; $Lines++) {
+	
+	    [Console]::SetCursorPosition(0,($CurrentLine - $Lines))
+	    [Console]::Write("{0,-$ConsoleWidth}" -f " ")
+
+    }
+
+    [Console]::SetCursorPosition(0,($CurrentLine - $Count))
+}
