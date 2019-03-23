1a6169728bf73751ec258396c69a10e2ee350215
Ben Therien <cs135cid@yahoo.com>
2019-03-23 12:33:34 -0400

    Update SuperReadKey_DEMO.ps1

--- SuperReadKey_DEMO.ps1
+++ SuperReadKey_DEMO.ps1
@@ -14,24 +14,27 @@ Write-Host "3 = Relax"
 Write-Host "4 = Don't Do It"
 Write-Host "5 = When you want to go to it"
 Write-Host "6 = Order Bartle Skeets"
-Write-Host "a = Neuter all racists"
+Write-Host "a = Can' we all get along?"
 Write-Host "b = World Peace"
 Write-Host "c = End Poverty"
 Write-Host ""
 
 IF ($host.name -eq 'Windows Powershell ISE Host') {ReadHost} #Used when you are in ISE.  Will necessitate an ENTER Key.
-IF ($host.name -eq 'ConsoleHost') {SuperReadKey -Mode Single -TimeToWait 10 -ValidChoicesList "1,2,3,4,5,6,a,b,c"} #Used when running in Console. This will NOT necessitate an ENTER Key. BUT, it ## will NOT work ## in ISE    
+IF ($host.name -eq 'ConsoleHost') {SuperReadKey -TimeToWait 10 -ValidChoicesList "1,2,3,4,5,6,a,b,c" -SendKeystrokeTo MenuActions} #Used when running in Console. This will NOT necessitate an ENTER Key. BUT, it ## will NOT work ## in ISE    
 
-Switch ($K) {
-    1 {Write-Host "The Numbers are: 4-8-19-27-34-10";pause}
-    2 {Write-Host "Invest!  Compound Interest is your Friend!";pause}
-    3 {Write-Host "Frankie went to Hollywood";pause}
-    4 {Write-Host "Frankie went to Hollywood";pause}
-    5 {Write-Host "Frankie went to Hollywood";pause}
-    6 {Write-Host "Search for Cleetus McFarland on Youtube";pause}
-    a {Write-Host "You know its the only way to stop the hate... don't let them breed";pause}
-    b {Write-Host "Get along so we can all move ahead.";pause}
-    c {Write-Host "Help your neighbor, pay it forward.";pause}
-    q {quit}
-    }
+Function MenuActions {
+    Switch ($K) {
+        1 {Write-Host "The Numbers are: 4-8-19-27-34-10";pause}
+        2 {Write-Host "Invest!  Compound Interest is your Friend!";pause}
+        3 {Write-Host "Frankie went to Hollywood";pause}
+        4 {Write-Host "Frankie went to Hollywood";pause}
+        5 {Write-Host "Frankie went to Hollywood";pause}
+        6 {Write-Host "Search for Cleetus McFarland on Youtube";pause}
+        a {Write-Host "I guess not.";pause}
+        b {Write-Host "Get along so we can all move ahead.";pause}
+        c {Write-Host "Help your neighbor, pay it forward.";pause}
+        q {quit}
+        }
+}
+
 
